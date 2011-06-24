8fdfd3ef689d23220ec3d07229304027a2cb8a50
Sébastien Briois <sebriois@gmail.com>
2011-06-24 12:06:19 +0200

    added a way to update bugs..

--- issues/urls.py
+++ issues/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import *
 
-from issues.views import index
+from issues.views import index, item, delete
 
 urlpatterns = patterns('',
+	url(r'^(?P<issue_id>\d+)/delete$', delete, name="issue_delete"),
+	url(r'^(?P<issue_id>\d+)/$', item, name="issue_item"),
   url(r'^$', index, name="issue_index")
 )
--- issues/views.py
+++ issues/views.py
@@ -11,10 +11,33 @@ from constants import *
 from utils import *
 
 @login_required
-@team_required
 @transaction.commit_on_success
 def index(request):
 	if request.method == 'POST':
 		form = IssueForm( data = request.POST )
 		if form.is_valid(): form.save()
 		return redirect('home')
+
+@login_required
+@transaction.commit_on_success
+def item(request, issue_id):
+	issue = get_object_or_404( Issue, id = issue_id )
+	if request.method == 'GET':
+		form = IssueForm( instance = issue )
+	elif request.method == 'POST':
+		form = IssueForm( data = request.POST, instance = issue )
+		if form.is_valid():
+			form.save()
+			return redirect('home')
+	
+	return direct_to_template( request, 'issues/item.html', {
+		'form': form,
+		'issue': issue
+	})
+
+@login_required
+@transaction.commit_on_success
+def delete(request, issue_id):
+	issue = get_object_or_404( Issue, id = issue_id )
+	issue.delete()
+	return redirect('home')
