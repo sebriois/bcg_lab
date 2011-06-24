a9e02718284a38f13a83c93649ae36be30b18d3f
Sébastien Briois <sebriois@gmail.com>
2011-06-24 11:09:32 +0200

    changed "comments" to "information"

--- infos/models.py
+++ infos/models.py
@@ -3,6 +3,6 @@
 from django.db import models
 
 class Info(models.Model):
-	text = models.TextField(u"Commentaire" )
+	text = models.TextField(u"Information" )
 	expiry = models.DateField(u"Expire le" )
 	date_created = models.DateField(u"Date", auto_now_add = True)
