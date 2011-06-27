0900eb1a9dc7b20a72db172aa3836d2a9cb11816
Sébastien Briois <sebriois@gmail.com>
2011-06-27 12:26:51 +0200

    fixed minor bugs

--- budget/models.py
+++ budget/models.py
@@ -5,71 +5,76 @@ from team.models import Team
 from constants import BUDGET_CHOICES
 
 class Budget(models.Model):
-  team = models.ForeignKey(Team, verbose_name="Equipe")
-  name = models.CharField(u"Nom", max_length=100, unique = True)
-  budget_type = models.IntegerField(u"Tutelle", choices = BUDGET_CHOICES)
-  default_credit_type = models.CharField(u"Type de crédit", max_length=30, null = True, blank = True)
-  default_nature = models.CharField(u"Nature", max_length=20, null = True, blank = True)
-  is_active = models.BooleanField(u"Actif?", default = True)
-  
-  class Meta:
-    verbose_name = "Budget"
-    verbose_name_plural = "Budgets"
-    ordering = ("team", "name")
-  
-  def __unicode__(self):
-    return u"%s (dispo: %s)" % ( self.name, self.get_amount_left() )
-  
-  def get_amount_left(self):
-    amount_left = 0
-    for line in BudgetLine.objects.filter( budget_id = self.id ):
-      if line.credit:
-        amount_left += line.credit
-      
-      if line.debit:
-        amount_left -= line.debit
-      
-    return amount_left
-  
-
+	team = models.ForeignKey(Team, verbose_name="Equipe")
+	name = models.CharField(u"Nom", max_length=100, unique = True)
+	budget_type = models.IntegerField(u"Tutelle", choices = BUDGET_CHOICES)
+	default_credit_type = models.CharField(u"Type de crédit", max_length=30, null = True, blank = True)
+	default_nature = models.CharField(u"Nature", max_length=20, null = True, blank = True)
+	is_active = models.BooleanField(u"Actif?", default = True)
+	
+	class Meta:
+		verbose_name = "Budget"
+		verbose_name_plural = "Budgets"
+		ordering = ("team", "name")
+	
+	def __unicode__(self):
+		return u"%s (dispo: %s)" % ( self.name, self.get_amount_left() )
+	
+	def get_amount_left(self):
+		amount_left = 0
+		for line in BudgetLine.objects.filter( budget_id = self.id ):
+			if line.credit:
+				amount_left += line.credit
+			
+			if line.debit:
+				amount_left -= line.debit
+			
+		return amount_left
+	
+	def update_budget_lines_team(self):
+		for bl in BudgetLine.objects.filter( budget_id = self.id ):
+			if bl.team != self.team.name:
+				bl.team = self.team.name
+				bl.save()
+	
 
 class BudgetLine(models.Model):
-  team          = models.CharField(u"Equipe", max_length = 100)
-  order_id      = models.IntegerField( u"ID de commande", null = True, blank = True )
-  orderitem_id  = models.IntegerField( u"ID d'item de commande", null = True, blank = True )
-  budget_id     = models.IntegerField( u"ID de budget" )
-  budget        = models.CharField(u"Budget", max_length = 100)
-  number        = models.CharField(u"N° de cde", max_length = 30, null = True, blank = True)
-  date          = models.DateTimeField(u"Date de l'acte", auto_now_add = True)
-  nature        = models.CharField(u"Nature", max_length = 20)
-  budget_type   = models.IntegerField(u"Tutelle", choices = BUDGET_CHOICES)
-  credit_type   = models.CharField(u"Type crédit", max_length = 40)
-  provider      = models.CharField(u"Fournisseur", max_length = 100)
-  offer         = models.CharField(u"Offre", max_length = 100, null = True, blank = True )
-  product       = models.CharField(u"Désignation", max_length = 100, null = True, blank = True )
-  ref           = models.CharField(u"Réf", max_length = 50, null = True, blank = True )
-  quantity      = models.IntegerField(u"Qté", null = True, blank = True, default = 1)
-  product_price = models.DecimalField(u"Montant", max_digits=12, decimal_places=2, null = True, blank = True)
-  credit        = models.DecimalField(u"Crédit", max_digits=12, decimal_places=2, null = True, blank = True)
-  debit         = models.DecimalField(u"Débit", max_digits=12, decimal_places=2, null = True, blank = True)
-  confidential  = models.BooleanField(u"Donnée confidentielle", default = False)
-  
-  class Meta:
-    verbose_name = "Ligne budgétaire"
-    verbose_name = "Lignes budgétaires"
-    ordering = ("team", "budget_id", "-order_id", "date")
-  
-  def get_amount_left(self):
-    amount_left = 0
-    for line in BudgetLine.objects.filter( team = self.team, budget = self.budget ):
-      if line.credit:
-        amount_left += line.credit
-      
-      if line.debit:
-        amount_left -= line.debit
-      
-      if line == self:
-        break
-    
-    return amount_left
-  
+	team					= models.CharField(u"Equipe", max_length = 100)
+	order_id			= models.IntegerField( u"ID de commande", null = True, blank = True )
+	orderitem_id	= models.IntegerField( u"ID d'item de commande", null = True, blank = True )
+	budget_id			= models.IntegerField( u"ID de budget" )
+	budget				= models.CharField(u"Budget", max_length = 100)
+	number				= models.CharField(u"N° de cde", max_length = 30, null = True, blank = True)
+	date					= models.DateTimeField(u"Date de l'acte", auto_now_add = True)
+	nature				= models.CharField(u"Nature", max_length = 20)
+	budget_type		= models.IntegerField(u"Tutelle", choices = BUDGET_CHOICES)
+	credit_type		= models.CharField(u"Type crédit", max_length = 40)
+	provider			= models.CharField(u"Fournisseur", max_length = 100)
+	offer					= models.CharField(u"Offre", max_length = 100, null = True, blank = True )
+	product				= models.CharField(u"Désignation", max_length = 100, null = True, blank = True )
+	ref						= models.CharField(u"Réf", max_length = 50, null = True, blank = True )
+	quantity			= models.IntegerField(u"Qté", null = True, blank = True, default = 1)
+	product_price = models.DecimalField(u"Montant", max_digits=12, decimal_places=2, null = True, blank = True)
+	credit				= models.DecimalField(u"Crédit", max_digits=12, decimal_places=2, null = True, blank = True)
+	debit					= models.DecimalField(u"Débit", max_digits=12, decimal_places=2, null = True, blank = True)
+	confidential	= models.BooleanField(u"Donnée confidentielle", default = False)
+	
+	class Meta:
+		verbose_name = "Ligne budgétaire"
+		verbose_name = "Lignes budgétaires"
+		ordering = ("team", "budget_id", "-order_id", "date")
+	
+	def get_amount_left(self):
+		amount_left = 0
+		for line in BudgetLine.objects.filter( team = self.team, budget = self.budget ):
+			if line.credit:
+				amount_left += line.credit
+			
+			if line.debit:
+				amount_left -= line.debit
+			
+			if line == self:
+				break
+		
+		return amount_left
+	
--- team/views_team.py
+++ team/views_team.py
@@ -19,83 +19,89 @@ from utils import is_admin
 @login_required
 @transaction.commit_on_success
 def index(request):
-  if request.method == 'GET':  return _team_list(request)
-  if request.method == 'POST': return _team_creation(request)
+	if request.method == 'GET':	 return _team_list(request)
+	if request.method == 'POST': return _team_creation(request)
 
 @login_required
 @transaction.commit_on_success
 def item(request, team_id):
-  team = get_object_or_404(Team, id = team_id)
-  if request.method == 'GET': return _team_detail(request, team)
-  if request.method == 'PUT': return _team_update(request, team)
+	team = get_object_or_404(Team, id = team_id)
+	if request.method == 'GET': return _team_detail(request, team)
+	if request.method == 'PUT': return _team_update(request, team)
 
 @login_required
 @superuser_required
 def new(request):
-  return direct_to_template(request, 'team/form.html', {
-    'form': TeamForm()
-  })
+	return direct_to_template(request, 'team/form.html', {
+		'form': TeamForm()
+	})
 
 @login_required
 def add_user_to_team(request, user_id):
-  user = get_object_or_404( User, id = user_id )
-  
-  team_id = request.GET.get( 'team_id', None )
-  if team_id:
-    team = get_object_or_404( Team, id = team_id )
-    member = TeamMember.objects.create(
-      user = user,
-      team = team
-    )
-  return redirect( 'team_index' )
+	user = get_object_or_404( User, id = user_id )
+	
+	team_id = request.GET.get( 'team_id', None )
+	if team_id:
+		team = get_object_or_404( Team, id = team_id )
+		member = TeamMember.objects.create(
+			user = user,
+			team = team
+		)
+	return redirect( 'team_index' )
 
 
 #--- Private views
 def _team_list(request):
-  noteam = []
-  for user in User.objects.all():
-    if user.teammember_set.all().count() == 0:
-      noteam.append( user )
-  
-  if request.user.is_superuser or is_admin(request.user):
-    teams = Team.objects.all()
-  else:
-    teams = [m.team for m in request.user.teammember_set.all()]
-  
-  return direct_to_template(request, 'team/index.html',{
-      'team_list': teams,
-      'noteam': noteam
-  })
+	noteam = []
+	for user in User.objects.all():
+		if user.teammember_set.all().count() == 0:
+			noteam.append( user )
+	
+	if request.user.is_superuser or is_admin(request.user):
+		teams = Team.objects.all()
+	else:
+		teams = [m.team for m in request.user.teammember_set.all()]
+	
+	return direct_to_template(request, 'team/index.html',{
+			'team_list': teams,
+			'noteam': noteam
+	})
 
 def _team_detail(request, team):
-  form = TeamForm(instance = team)
-  return direct_to_template(request, 'team/item.html',{
-      'team': team,
-      'form': form
-  })
+	form = TeamForm(instance = team)
+	return direct_to_template(request, 'team/item.html',{
+			'team': team,
+			'form': form
+	})
 
 def _team_creation(request):
-  form = TeamForm(request.POST)
-  if form.is_valid():
-    team = form.save()
-    
-    info_msg( request, u"Equipe ajoutée avec succès." )
-    return redirect( 'team_index' )
-  else:
-    return direct_to_template(request, 'team/form.html',{
-        'form': form
-    })
+	form = TeamForm(request.POST)
+	if form.is_valid():
+		team = form.save()
+		
+		info_msg( request, u"Equipe ajoutée avec succès." )
+		return redirect( 'team_index' )
+	else:
+		return direct_to_template(request, 'team/form.html',{
+				'form': form
+		})
 
 def _team_update(request, team):
-  form = TeamForm(instance = team, data = request.REQUEST)
-  if form.is_valid():
-    team = form.save()
-    
-    info_msg( request, u"Equipe modifiée avec succès." )
-    return redirect( 'team_index' )
-  else:
-    return direct_to_template(request, 'team/item.html',{
-        'team': team,
-        'form': form
-    })
-  
+	form = TeamForm(instance = team, data = request.REQUEST)
+	name_before = team.name
+	
+	if form.is_valid():
+		team = form.save()
+		
+		if name_before != team.name:
+			for budget in team.budget_set.all():
+				budget.update_budget_lines_team()
+		
+		info_msg( request, u"Equipe modifiée avec succès." )
+		return redirect( 'team_index' )
+	else:
+		return direct_to_template(request, 'team/item.html',{
+				'team': team,
+				'form': form
+		})
+	
