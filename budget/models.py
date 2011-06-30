433cbd967df458c7f71831151998828c61c91e05
Sébastien Briois <sebriois@gmail.com>
2011-06-30 09:40:28 +0200

    ordering budgets

--- budget/models.py
+++ budget/models.py
@@ -62,7 +62,7 @@ class BudgetLine(models.Model):
 	class Meta:
 		verbose_name = "Ligne budgétaire"
 		verbose_name = "Lignes budgétaires"
-		ordering = ("team", "budget_id", "-order_id", "date")
+		ordering = ("team", "budget_id", "date", "-order_id")
 	
 	def get_amount_left(self):
 		amount_left = 0
