9255ec1979a391967efa5ba1108ca9e0e06df2b6
Sébastien Briois <sebriois@gmail.com>
2011-09-20 10:51:01 +0200

    added budget admin

---
+++ budget/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from budget.models import Budget, BudgetLine
+
+class BudgetAdmin(admin.ModelAdmin):
+  list_display = ('team', 'name', 'budget_type', 'default_origin', 'default_nature', 'tva_code', 'domain', 'is_active')
+  list_display_links = ('name',)
+admin.site.register(Budget, BudgetAdmin)
+
+class BudgetLineAdmin(admin.ModelAdmin):
+  list_display = ('team', 'date', 'budget', 'number', 'budget_type', 'nature')
+  list_display_links = ('date',)
+admin.site.register(BudgetLine, BudgetLineAdmin)
