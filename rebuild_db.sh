e801bbea192051c20b839b6f40b77ae248f775e1
Sébastien Briois <sebriois@gmail.com>
2011-10-21 10:21:51 +0200

    added database dump before rebuilding DB

--- rebuild_db.sh
+++ rebuild_db.sh
@@ -1,4 +1,5 @@
 sudo /usr/sbin/apachectl stop
+python manage.py dumpdata --indent=2 > initial_data.json
 dropdb order_manager -U briois
 createdb order_manager -U briois -E utf8
 python manage.py syncdb --no
