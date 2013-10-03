6edfe84601e1a80c3ad14bb0839219aed9f22026
Sebastien Briois <sebriois@gmail.com>
2013-10-03 21:38:41 +0200

    fixed typo in settings

--- bcg_lab/settings.py
+++ bcg_lab/settings.py
@@ -31,7 +31,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/var/www/bcg_lab/media' )
+MEDIA_ROOT = '/var/www/bcg_lab/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
