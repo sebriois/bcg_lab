bc11adab2ae5db37c97ea81fc63eef52d39924c5
Sébastien Briois <sebriois@gmail.com>
2017-12-17 20:23:40 +0100

    added teamnamehistory to admin

--- team/admin.py
+++ team/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from team.models import Team, TeamMember
+from team.models import Team, TeamMember, TeamNameHistory
 
 
 class TeamAdmin(admin.ModelAdmin):
@@ -10,3 +10,8 @@ admin.site.register(Team, TeamAdmin)
 class TeamMemberAdmin(admin.ModelAdmin):
   list_display = ('user', 'team')
 admin.site.register(TeamMember, TeamMemberAdmin)
+
+
+class TeamNameHistoryAdmin(admin.ModelAdmin):
+  list_display = ('team', 'name', 'fullname')
+admin.site.register(TeamNameHistory, TeamNameHistoryAdmin)
