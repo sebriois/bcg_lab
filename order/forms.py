e63bdd0184a3382975693b1c111d557e9d15ef1d
Sébastien Briois <sebriois@gmail.com>
2017-12-20 17:37:35 +0100

    Closes #91

--- order/forms.py
+++ order/forms.py
@@ -18,12 +18,12 @@ class OrderItemForm(forms.ModelForm):
         exclude = ('product_id', 'cost_type')
         widgets = {
             'item_type': widgets.HiddenInput(),
-            'nomenclature': widgets.TextInput(attrs={
+            'nomenclature': widgets.TextInput(attrs = {
                 'class': 'autocomplete',
                 'autocomplete_url': reverse_lazy('product_code:autocomplete')
             })
         }
-    
+
     def clean_nomenclature(self):
         nomenclature = self.cleaned_data.get('nomenclature', None)
         if not nomenclature and self.data.get('item_type', None) == "0":
@@ -54,20 +54,16 @@ class AddCreditForm(forms.ModelForm):
 
     def clean_price(self):
         price = self.cleaned_data.get('price', None)
-        try:
-            price = Decimal(price.replace(',','.'))
-        except InvalidOperation:
-            raise forms.ValidationError(u"Veuillez saisir un montant positif.")
-        
-        if price <= 0:
-            raise forms.ValidationError(u"Veuillez saisir un montant positif.")
-        
+
+        if not price > 0:
+            raise forms.ValidationError(u"Veuillez saisir un nombre décimal positif.")
+
         return price
 
 
 class AddDebitForm(forms.ModelForm):
-    price = forms.CharField( label = u"Montant" )
-    
+    price = forms.CharField(label = u"Montant")
+
     class Meta:
         model = OrderItem
         fields = ("name", "reference", "offer_nb", "price", "quantity", "cost_type")
@@ -84,20 +80,16 @@ class AddDebitForm(forms.ModelForm):
 
     def clean_price(self):
         price = self.cleaned_data.get('price', None)
-        try:
-            price = Decimal(price.replace(',','.'))
-        except InvalidOperation:
-            raise forms.ValidationError(u"Veuillez saisir un montant positif.")
-        
-        if price <= 0:
-            raise forms.ValidationError(u"Veuillez saisir un montant positif.")
-        
+
+        if not price > 0:
+            raise forms.ValidationError(u"Veuillez saisir un nombre décimal positif.")
+
         return price
-    
+
 
 class FilterForm(forms.Form):
     connector = forms.TypedChoiceField(
-        choices = [("AND",u"toutes les"), ("OR", u"l'une des")],
+        choices = [("AND", u"toutes les"), ("OR", u"l'une des")],
         initial = "AND",
         coerce = str,
         empty_value = None,
@@ -106,33 +98,33 @@ class FilterForm(forms.Form):
 
     team = forms.ModelChoiceField(
         queryset = Team.objects.all(),
-        label    = "Equipe",
+        label = "Equipe",
         required = False
     )
-    
+
     number = forms.CharField(
-        label    = u"N°cmde",
-        widget   = widgets.TextInput( attrs = { 
-            'class' : 'autocomplete', 
+        label = u"N°cmde",
+        widget = widgets.TextInput(attrs = {
+            'class': 'autocomplete',
             'autocomplete_url': reverse_lazy('order:autocomplete_number')
         }),
         required = False
     )
-    
+
     provider = forms.ModelChoiceField(
-        queryset = Provider.objects.exclude( is_local = True ),
-        label    = "Fournisseur",
+        queryset = Provider.objects.exclude(is_local = True),
+        label = "Fournisseur",
         required = False
     )
     notes__icontains = forms.CharField(
-        label    = u"Commentaire",
+        label = u"Commentaire",
         required = False
     )
-    
+
     items__name__icontains = forms.CharField(
-        label    = u"Produit",
-        widget   = widgets.TextInput( attrs = { 
-            'class' : 'autocomplete', 
+        label = u"Produit",
+        widget = widgets.TextInput(attrs = {
+            'class': 'autocomplete',
             'autocomplete_url': reverse_lazy('product:autocomplete')
         }),
         required = False
@@ -145,49 +137,49 @@ class ServiceForm(forms.Form):
         queryset = Team.objects.all()
     )
     provider = forms.ModelChoiceField(
-        label = u"Type de service", 
+        label = u"Type de service",
         queryset = Provider.objects.filter(is_service = True),
         required = True
     )
-    name         = forms.CharField( label = u"Désignation" )
+    name = forms.CharField(label = u"Désignation")
     nomenclature = forms.CharField(
-        label    = u"Nomenclature",
+        label = u"Nomenclature",
         required = False,
-        widget   = widgets.TextInput( attrs = { 
-            'class' : 'autocomplete', 
+        widget = widgets.TextInput(attrs = {
+            'class': 'autocomplete',
             'autocomplete_url': reverse_lazy('product_code:autocomplete')
         }),
     )
-    cost         = forms.CharField( label = u"Montant" )
-    quantity     = forms.IntegerField( label = u"Quantité", initial = 1 )
-    confidential = forms.BooleanField( label = u"Confidentiel ?", initial = False, required = False )
-    
+    cost = forms.CharField(label = u"Montant")
+    quantity = forms.IntegerField(label = u"Quantité", initial = 1)
+    confidential = forms.BooleanField(label = u"Confidentiel ?", initial = False, required = False)
+
     def __init__(self, member, *args, **kwargs):
-        super( ServiceForm, self ).__init__( *args, **kwargs )
-        
+        super(ServiceForm, self).__init__(*args, **kwargs)
+
         if not member.user.has_perm("order.custom_order_any_team"):
             self.fields['team'].initial = member.team
-            self.fields['team'].widget.attrs.update({ 'disabled': 'disabled' })
-    
+            self.fields['team'].widget.attrs.update({'disabled': 'disabled'})
+
     def clean_cost(self):
         cost = self.cleaned_data.get('cost', None)
         try:
-            cost = Decimal(cost.replace(',','.'))
+            cost = Decimal(cost.replace(',', '.'))
         except InvalidOperation:
             raise forms.ValidationError(u"Veuillez saisir un montant positif.")
-        
+
         if cost <= 0:
             raise forms.ValidationError(u"Veuillez saisir un montant positif.")
-        
+
         return cost
-    
+
     def clean_nomenclature(self):
         nomenclature = self.cleaned_data.get('nomenclature', None)
         if not nomenclature:
             return None
-        
+
         product_code = nomenclature.split(' - ')[0]
-        if ProductCode.objects.filter( code = product_code ).count() == 0:
+        if ProductCode.objects.filter(code = product_code).count() == 0:
             raise forms.ValidationError(u"Cette nomenclature (%s) n'est pas reconnue." % product_code)
-        
+
         return product_code
--- product/views.py
+++ product/views.py
@@ -24,48 +24,49 @@ from utils.request_messages import info_msg
 
 def autocomplete(request):
     q = request.GET.get('query', '')
-    
+
     suggestions = list(set(Product.objects.filter(name__icontains = q).only('name').values_list('name', flat = True)))
     suggestions.sort()
-    
+
     output_data = {
         'query': q,
         'suggestions': suggestions
     }
-    
+
     return HttpResponse(json.dumps(output_data))
 
 
 def _elastic_search(query_dict):
     es = Elasticsearch(hosts = settings.ELASTICSEARCH_HOSTS)
+
     query = {
-      "query": {
-        "filtered": {
-          "query": {
-            "query_string": {
-              "query": query_dict['q']
+        "query": {
+            "filtered": {
+                "query": {
+                    "query_string": {
+                        "query": query_dict['q']
+                    }
+                }
             }
-          }
+        },
+        "fields": [
+            "reference",
+            "nomenclature",
+            "provider",
+            "offer_nb",
+            "origin",
+            "name",
+            "category"
+        ],
+        "from": 50 * (int(query_dict.get("page", 1)) - 1),
+        "size": 50,
+        "sort": {
+            "_score": {
+                "order": "desc"
+            }
         }
-      },
-      "fields": [
-        "reference",
-        "nomenclature",
-        "provider",
-        "offer_nb",
-        "origin",
-        "name",
-        "category"
-      ],
-      "from": 50 * (int(query_dict.get("page", 1)) - 1),
-      "size": 50,
-      "sort": {
-        "_score": {
-          "order": "desc"
-        }
-      }
     }
-    res = es.search(index = settings.SITE_NAME.lower(), body = query, filter_path=['hits.total', 'hits.hits._id'])
+    res = es.search(index = settings.SITE_NAME.lower(), body = query, filter_path = ['hits.total', 'hits.hits._id'])
     product_list = Product.objects.filter(id__in = [hit['_id'] for hit in res['hits']['hits']])
 
     return product_list, res['hits']['total']
@@ -73,7 +74,7 @@ def _elastic_search(query_dict):
 
 def _django_search(query_dict):
     form = ProductFilterForm(data = query_dict)
-    
+
     if len(query_dict.keys()) > 1:
         if form.is_valid():
             data = dict()
@@ -83,17 +84,17 @@ def _django_search(query_dict):
 
             if 'id__in' in query_dict:
                 data['id__in'] = query_dict['id__in'].split(',')
-            
+
             if 'has_expired' in query_dict:
                 data['expiry__lt'] = timezone.now()
-            
+
             if 'is_valid' in query_dict:
                 data['expiry__gte'] = timezone.now()
-            
+
             Q_obj = Q()
             Q_obj.connector = data.pop("connector")
-            Q_obj.children  = data.items()
-            
+            Q_obj.children = data.items()
+
             product_list = Product.objects.filter(Q_obj)
             num_found = product_list.count()
         else:
@@ -102,7 +103,7 @@ def _django_search(query_dict):
     else:
         product_list = Product.objects.none()
         num_found = 0
-    
+
     return product_list, num_found
 
 
@@ -114,16 +115,16 @@ def _product_search(query_dict):
     else:
         product_list = Product.objects.none()
         num_found = 0
-    
+
     return product_list, num_found
 
 
 @login_required
 def index(request):
     query_dict = request.GET.copy()
-    
+
     product_list, num_found = _product_search(query_dict)
-    
+
     if request.user.has_perm("order.custom_view_local_provider") and not request.user.is_superuser:
         if not 'q' in query_dict.keys():
             product_list = Product.objects.filter(provider__is_local = True)
@@ -133,25 +134,25 @@ def index(request):
         product_count = Product.objects.filter(provider__is_local = True).count()
     else:
         product_count = Product.objects.all().count()
-    
+
     # Pagination
     if 'page' in query_dict:
         current_page = int(query_dict.pop('page')[0])
     else:
         current_page = 1
-    
+
     if not 'q' in query_dict.keys():
         start = (current_page - 1) * settings.PAGINATION_ROWS
-        end   = start + settings.PAGINATION_ROWS
+        end = start + settings.PAGINATION_ROWS
         if end > num_found:
             end = num_found
         product_list = product_list[start:end]
-    
+
     return render(request, 'product/index.html', {
         'product_count': product_count,
         'search_count': num_found,
         'filter_form': ProductFilterForm(),
-        'q_init': query_dict.get("q",""),
+        'q_init': query_dict.get("q", ""),
         'products': product_list,
         'current_page': current_page,
         'url_args': query_dict.urlencode(),
@@ -176,12 +177,12 @@ def item(request, product_id):
                 if orig_price != product.price and product.has_expired():
                     product.expiry = datetime.strptime("31/12/%s" % timezone.now().year, "%d/%m/%Y")
                     product.save()
-                    
+
                 info_msg(request, u"Produit modifié avec succès.")
             return redirect(reverse('product:list') + '?' + url_args[0])
-    
+
     product_type = ContentType.objects.get_for_model(Product)
-    return render(request, 'product/item.html',{
+    return render(request, 'product/item.html', {
         'product': product,
         'product_type': product_type.id,
         'form': form,
@@ -196,7 +197,7 @@ def new(request):
     form = ProductForm()
 
     if request.method == 'GET':
-        provider_id = request.GET.get('provider_id',None)
+        provider_id = request.GET.get('provider_id', None)
         if provider_id:
             provider = get_object_or_404(Provider, id = provider_id)
             form = ProductForm(provider = provider)
@@ -210,7 +211,7 @@ def new(request):
         else:
             provider = None
             form = ProductForm(data = request.POST)
-        
+
         if form.is_valid():
             p = form.save()
             if p.provider.reseller:
@@ -220,7 +221,7 @@ def new(request):
 
             info_msg(request, u"Produit ajouté avec succès.")
             return redirect(reverse('product:list') + "?reference=%s&connector=OR" % p.reference)
-    
+
     return render(request, 'product/new.html', {
         'provider': provider,
         'form': form
@@ -244,27 +245,27 @@ def delete(request, product_id):
 @login_required
 def export_xls(request):
     product_list, num_found = _product_search(request.GET.copy())
-    
+
     if request.user.has_perm("order.custom_view_local_provider"):
         if num_found == 0:
             product_list = Product.objects.all()
         product_list = product_list.filter(provider__is_local = True)
-    
+
     wb = xlwt.Workbook()
     ws = wb.add_sheet("export")
-    
-    header = [u"FOURNISSEUR", u"DESIGNATION", u"REFERENCE", u"PRIX", u"CONDITIONNEMENT", 
-    u"NOMENCLATURE", u"FOURNISSEUR D'ORIGINE", u"N°OFFRE", u"EXPIRATION", u"MISE A JOUR"]
+
+    header = [u"FOURNISSEUR", u"DESIGNATION", u"REFERENCE", u"PRIX", u"CONDITIONNEMENT",
+              u"NOMENCLATURE", u"FOURNISSEUR D'ORIGINE", u"N°OFFRE", u"EXPIRATION", u"MISE A JOUR"]
     for col, title in enumerate(header): ws.write(0, col, title)
-    
+
     row = 1
-    
+
     for product in product_list:
         if product.expiry:
             expiry = product.expiry.strftime("%d/%m/%Y")
         else:
             expiry = u""
-        
+
         ws.write(row, 0, product.provider.name)
         ws.write(row, 1, product.name)
         ws.write(row, 2, product.reference)
@@ -276,7 +277,7 @@ def export_xls(request):
         ws.write(row, 8, expiry)
         ws.write(row, 9, product.last_change.strftime("%d/%m/%Y"))
         row += 1
-    
+
     response = HttpResponse()
     response['Content-Disposition'] = 'attachment; filename=export_produit.xls'
     response['Content-Type'] = 'application/vnd.ms-excel; charset=utf-8'
@@ -284,50 +285,51 @@ def export_xls(request):
 
     return response
 
+
 @login_required
 @transaction.atomic
 def edit_list(request):
     if request.method == 'GET':
         product_list, num_found = _product_search(request.GET.copy())
-        
+
         if request.user.has_perm("order.custom_view_local_provider") and not request.user.is_superuser:
             product_list = product_list.filter(provider__is_local = True)
-        
+
         return render(request, 'product/edit_list.html', {
             'filter_form': ProductFilterForm(),
             'edit_form': EditListForm(),
             'product_list': product_list,
             'url_args': request.GET.urlencode()
         })
-    
+
     if request.method == 'POST':
         form = EditListForm(data = request.POST)
-        
+
         if form.is_valid():
             clean_data = form.cleaned_data
-            
+
             percent_raise = clean_data.get('percent_raise', None)
             category = clean_data.get('category', None)
             sub_category = clean_data.get('sub_category', None)
             nomenclature = clean_data.get('nomenclature', None)
-            offer_nb = clean_data.get('offer_nb',None)
-            expiry = clean_data.get('expiry',None)
-            
+            offer_nb = clean_data.get('offer_nb', None)
+            expiry = clean_data.get('expiry', None)
+
             product_ids = map(
-                lambda item: int(item.split("_")[1]), 
+                lambda item: int(item.split("_")[1]),
                 filter(
-                    lambda key: key.startswith("product_"), 
-                    request.POST.keys() 
+                    lambda key: key.startswith("product_"),
+                    request.POST.keys()
+                )
             )
-        )
-            
+
             product_list = Product.objects.filter(id__in = product_ids)
-            
+
             for product in product_list:
                 if request.POST['delete_all'] == "on":
                     product.delete()
                     continue
-                
+
                 if category: product.category = category
                 if sub_category: product.sub_category = sub_category
                 if nomenclature: product.nomenclature = nomenclature.strip()
@@ -336,7 +338,7 @@ def edit_list(request):
                 if offer_nb: product.offer_nb = offer_nb
                 if expiry: product.expiry = expiry
                 product.save()
-            
+
             if request.POST['delete_all'] == "on":
                 info_msg(request, "Produits supprimés avec succès.")
                 return redirect(reverse('product:edit_list') + "?" + request.POST["url_args"])
@@ -346,7 +348,7 @@ def edit_list(request):
         else:
             url_args = dict(parse_qsl(request.POST["url_args"]))
             product_list, filter_form = _product_search(url_args)
-            
+
             return render(request, 'product/edit_list.html', {
                 'edit_form': form,
                 'product_list': product_list,
